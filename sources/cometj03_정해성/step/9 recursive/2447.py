0ab6ff39aa07d86fb3e3960b9b24facc9bb5e355
정해성 <harryjung1001@gmail.com>
2022-04-19 20:38:26 +0900

    0419

--- sources/cometj03_정해성/step/9 recursive/2447.py
+++ sources/cometj03_정해성/step/9 recursive/2447.py
@@ -1,2 +1,21 @@
 # 2447 별 찍기 - 10
+N = int(input())
+arr = [[' ']*N for _ in range(N)]
 
+def star(x, y, n):
+  if n == 1:
+    arr[x][y] = '*'
+    return
+  for i in range(3):
+    for j in range(3):
+      if i == j == 1:
+        continue
+      t = n//3
+      star(x + t*j, y + t*i, t)
+
+
+star(0, 0, N)
+for row in arr:
+  for e in row:
+    print(e, end='')
+  print()
